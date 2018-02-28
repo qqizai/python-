d25e6d706f0e5985d21994533288e3ac285d6a91
xinlingqudongX <chenpeng10810@gmail.com>
2018-02-28 14:56:37 +0800

    测试任务

---
+++ zhaopinweb/测试爬虫.py
@@ -0,0 +1,92 @@
+import requests
+import re
+from lxml import etree
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+
+class Tspider(object):
+
+    def __init__(self):
+        self.request = requests.Session()
+        self.head = {'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+                    "authority":"www.endclothing.com",
+                    "method":"GET",
+                    "path":'/row/neighborhood-drizzler-jacket-181spnh-jkm01-grn.html',
+                    'scheme':'https',
+                    'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'accept-encoding':'gzip, deflate, br',
+                    'accept-language':'zh,zh-CN;q=0.9',
+                    'cache-control':'max-age=0',
+                    'dnt':'1',
+                    'upgrade-insecure-requests':'1'}
+
+    def get(self,url):
+        response = self.request.get(url,headers=self.head)
+        return response.content
+    
+    def data(self,response):
+        html = etree.HTML(response)
+        data = html.xpath('//div[@class="product-item-info"]/a')
+        return data
+    
+    def test(self,response):
+        with open('test.html','w+') as f:
+            f.write(response)
+
+
+class T2spider(object):
+
+    def __init__(self,path=None,headless=False):
+        options = webdriver.FirefoxOptions()
+        if headless:
+            options.set_headless()
+            options.add_argument('--disable-gpu')
+        self.web = webdriver.Firefox(firefox_options=options,firefox_binary=path)
+    
+    def get_list(self,url):
+        ls = []
+        self.web.get(url)
+        item = WebDriverWait(self.web,20).until(lambda driver:driver.find_elements_by_xpath('//div[@class="product-item-info"]/a'))
+        if item:
+            return map(lambda x:x.get_attribute('href'),item)
+        else:
+            return None
+
+    def get_data(self,url):
+        ls = []
+        self.web.get(url)
+        #time.sleep(20)
+        span = WebDriverWait(self.web,20).until(lambda driver:driver.find_element_by_xpath('//span[@class="base"]'))
+        imgs = WebDriverWait(self.web,20).until(lambda driver:driver.find_elements_by_xpath('//div[@class="fotorama__thumb fotorama_vertical_ratio fotorama__loaded fotorama__loaded--img"]/img'))
+        
+        #print imgs
+        #print imgs[0].get_attribute('src')
+        # if span or imgs:
+        #     print 'Yes'
+        #     print span.text
+        #     print imgs
+        # else:
+        #     print u'None 未找到数据'
+        ls.append(map(lambda x:x.get_attribute('src'),imgs))
+        #span = self.web.find_element_by_xpath('//span[@class="base"]')
+        ls.append(span.text)
+        price = self.web.find_element_by_xpath('//span[@class="price"]')
+        ls.append(price.text)
+        desc = self.web.find_elements_by_xpath('//div[@class="value"]')
+        ls.extend(map(lambda x:x.text,desc))
+
+        return ls
+
+
+if __name__ == '__main__':
+    spider = T2spider()
+    item = spider.get_list('https://www.endclothing.com/row/clothing')
+    if item:
+        for i in item:
+            response = spider.get_data(i)
+            print response
+
+
+
